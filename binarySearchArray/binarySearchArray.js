5c32530bbd605ca00f54a16168c75395e2d575e2
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-01-25 14:23:26 +0200

    not yet

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,6 +11,22 @@
  */
 
 var binarySearch = function (array, target) {
+	var middle = Math.ceil((array.length-1)/2)
+	// var first =0,last=array.length-1
+	// while(){}
+	for (var i = 0; i < array.length; i++) {
+		// if (target >array[Math.ceil((array.length-1)/2)]) {
+		// }
+		if (array[middle] > target) {
+			array.slice(0,-(middle)-1)
+			middle = Math.ceil((array.length-1)/2)
+		}else if (array[middle] < target) {
+			array = array.slice(mdiddle+1)
+			middle = Math.ceil((array.length-1)/2)
+
+		}
+	}
+
   
 };
 
