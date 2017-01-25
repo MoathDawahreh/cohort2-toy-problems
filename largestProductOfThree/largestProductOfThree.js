793157a6f6221cdcfd3307b1c2515f375e2ebf0c
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-01-25 14:50:08 +0200

    solved

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,9 +8,15 @@
  */
 
 var largestProductOfThree = function(array) {
-	
+	num1 = max(array);
+	array2 = array.slice(0,array.indexOf(num1))
+	num2 = max(array2);
+	array3 =array2.slice(0,array.indexOf(num2))
+	num3 = max(array3);
+	return num1*num2*num3
 };
 
 
-
-
+function max(list){
+	return Math.max.apply(null, list);
+}
