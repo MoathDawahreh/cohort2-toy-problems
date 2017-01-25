7962533caf99563e9083c86c591c37a0a2924170
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-01-25 14:30:28 +0200

    handle negative

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,11 +7,28 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-
+// handle negative numbers
  var largestProductOfThree = function(array) {
+ 	var count = 0
  	array.sort()
- 	return array[array.length-1]*array[array.length-2]*array[array.length-3]
-};
+ 	for (var i = 0; i < array.length; i++) {
+ 		if (array[i]<0) {
+ 			count++
+ 		}
+ 	}
+ 	if (count<2) {
+	 	return array[array.length-1]*array[array.length-2]*array[array.length-3]
+ 	}
+ 	if (count > 1) {
+ 		return array[0]*array[1]*array[array.length-1]
+ 	}
+
+}
+
+//  var largestProductOfThree = function(array) {
+//  	array.sort()
+//  	return array[array.length-1]*array[array.length-2]*array[array.length-3]
+// };
 
 // var largestProductOfThree = function(array) {
 // 	num1 = max(array);
