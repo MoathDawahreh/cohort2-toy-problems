c1df146336437c7d08a0686378490bad90c28773
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-01-25 14:57:15 +0200

    s

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,16 +7,22 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-var largestProductOfThree = function(array) {
-	num1 = max(array);
-	array2 = array.slice(0,array.indexOf(num1))
-	num2 = max(array2);
-	array3 =array2.slice(0,array.indexOf(num2))
-	num3 = max(array3);
-	return num1*num2*num3
+
+ var largestProductOfThree = function(array) {
+ 	array.sort()
+ 	return array[array.length-1]*array[array.length-2]*array[array.length-3]
 };
 
+// var largestProductOfThree = function(array) {
+// 	num1 = max(array);
+// 	array2 = array.slice(0,array.indexOf(num1))
+// 	num2 = max(array2);
+// 	array3 =array2.slice(0,array.indexOf(num2))
+// 	num3 = max(array3);
+// 	return num1*num2*num3
+// };
 
-function max(list){
-	return Math.max.apply(null, list);
-}
+
+// function max(list){
+// 	return Math.max.apply(null, list);
+// }
