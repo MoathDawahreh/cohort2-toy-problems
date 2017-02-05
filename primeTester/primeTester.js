1ce994ee4ac6be35d51823a2661db109997e3d93
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-02-05 15:52:44 +0200

    solved

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -3,10 +3,17 @@
  * except itself and 1. Write a function that accepts a number and returns true if it's
  * a prime number, false if it's not.
  */
-var primeTester = function(n) {
-
+var primeTester = function(n)  {
+	if (n < 2) {
+		return false
+	}
+    for(var i = 2; i < n; i++) {
+        if(n % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
  /* Extra credit: Write a function that generates a list of all prime numbers
   * in a user-specified range (inclusive). If you're not quite sure where to start,
   * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
